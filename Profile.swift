51864ad650ccac2997dbcf22da9c457795007fc2
steven <steven@burrows.plus.com>
2020-06-12 15:51:27 +0100

    finished class

---
+++ Profile.swift
@@ -0,0 +1,9 @@
+//
+//  Profile.swift
+//  superheros_socal
+//
+//  Created by Steven Burrows on 12/06/2020.
+//  Copyright © 2020 Steven Burrows. All rights reserved.
+//
+
+import Foundation
---
+++ superheros_socal/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  superheros_socal
+//
+//  Created by Steven Burrows on 12/06/2020.
+//  Copyright © 2020 Steven Burrows. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
